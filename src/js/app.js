252f9c29a56ae57604877cafcdfdacb513dbc569
Idan Zaharenko <idanzahar@gmail.com>
2021-04-21 00:45:52 -0500

    added variables

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,18 @@
-const mapBoxToken = "pk.eyJ1IjoiaWRhbnphaGEiLCJhIjoiY2tucDBpbTdkMWFsdzJ2cGVramh3cGJ4MCJ9.oewmkBX2H-FTDOusd-Y1Yg";
-const origin = document.querySelector('.origin-form');
-const origins = document.querySelector('.origins');
-const destinations = document.querySelector('.destinations');
-const busCont = document.querySelector('.bus-container');
-
-destinations.innerHTML = "";
-origins.innerHTML = "";
-busCont.innerHTML = "";
-
-async function inputLocations() {
-  const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/types?access_token=${mapBoxToken}`);
-  const originJSON = await response.json();
-
-  console.log(response);
-}
+let mapURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/";
+let busURL =  "https://api.winnipegtransit.com/v3/trip-planner.json?api-key=";
+let transitKey = "GtccxA20vtH5vxIYeR36&";
+let mapKey = "access_token=pk.eyJ1IjoiaWRhbnphaGEiLCJhIjoiY2tucDBpbTdkMWFsdzJ2cGVramh3cGJ4MCJ9.oewmkBX2H-FTDOusd-Y1Yg";
 
 
-origin.onsubmit = e => {
-  e.preventDefault();
-  if(origin !== "") {
-  origins.insertAdjacentHTML('afterbegin', `     
-  <li data-long="-97.113936" data-lat="49.823059">
-  <div class="name">WORKS</div>
-  <div> 1588 St. Mary's Road</div>
-</li>`)
-  } else {
-    console.log("works");
-  }
-}
+const orgForm = document.querySelector('.origin-form');
+const orgDiv = document.querySelector('.origins');
+const orgInp = document.querySelector('.origin-form input');
+const desForm = document.querySelector('.destination-form');
+const desDiv = document.querySelector('.destinations');
+const desInp = document.querySelector('.destination-form input');
+const butt = document.querySelector('button');
+const tripSec = document.querySelector('.my-trip');
+const selDiv = document.getElementsByClassName("selected");
 
 
 
